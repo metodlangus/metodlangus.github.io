03e4fc4af53b023a0c4b97b5a947971f3e65c8b9
Metod Langus <120268884+metodlangus@users.noreply.github.com>
2024-03-01 13:54:26 +0100

    Create leaflet-download-gpx-button.js

---
+++ scripts/leaflet-download-gpx-button.js
@@ -0,0 +1,74 @@
+//<![CDATA[ 
+// Custom Leaflet Control for Downloading GPX Files
+L.Control.DownloadGPX = L.Control.extend({
+    options: {
+        position: "topleft",
+        title: "Download GPX",
+        gpxData: null,
+        fileName: null
+    },
+    onAdd: function(map) {
+        var container = L.DomUtil.create("div", "leaflet-control-download-gpx leaflet-bar leaflet-control");
+        this.link = L.DomUtil.create("a", "leaflet-control-download-gpx-button leaflet-bar-part", container);
+        this.link.href = "#";
+        this._map = map;
+        this.link.title = this.options.title;
+        L.DomEvent.on(this.link, "click", this._click, this);
+        return container;
+    },
+    _click: function(e) {
+        L.DomEvent.stopPropagation(e);
+        L.DomEvent.preventDefault(e);
+        this.downloadGPX();
+    },
+    downloadGPX: function() {
+        var gpxData = this.options.gpxData;
+        var fileName = this.options.fileName;
+        
+        if (!gpxData) {
+            console.error("GPX data not provided.");
+            return;
+        }
+        
+        // Create a Blob containing the GPX data
+        var blob = new Blob([gpxData], {type: 'application/gpx+xml'});
+        
+        // Create a temporary anchor element
+        var a = document.createElement('a');
+        a.href = window.URL.createObjectURL(blob);
+        
+        // Set the file name
+        if (fileName) {
+            a.download = fileName + '.gpx';
+        } else {
+            console.error("File name not provided.");
+            return;
+        }
+        
+        // Append the anchor to the body and trigger the click event
+        document.body.appendChild(a);
+        a.click();
+        
+        // Clean up
+        document.body.removeChild(a);
+    }
+});
+
+// Add DownloadGPX Control to Map options
+L.Map.mergeOptions({
+    downloadGPXControl: false
+});
+
+// Initialization hook for DownloadGPX Control
+L.Map.addInitHook(function() {
+    if (this.options.downloadGPXControl) {
+        this.downloadGPXControl = new L.Control.DownloadGPX(this.options.downloadGPXControl);
+        this.addControl(this.downloadGPXControl);
+    }
+});
+
+// Function for creating DownloadGPX Control
+L.control.downloadGPX = function(options) {
+    return new L.Control.DownloadGPX(options);
+};
+//]]>
