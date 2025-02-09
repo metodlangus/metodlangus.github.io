888e5eeea011aba73768aa10d7eb7f9d3cb8abb3
Metod Langus <metod.langus@iskraemeco.com>
2025-02-09 18:23:12 +0100

    Update get_photos_content.js

--- get_photos_content.js
+++ get_photos_content.js
@@ -29,13 +29,17 @@ function fetchData() {
                 }
 
                 const output = uniqueImages
-                    .map(img => `${img.imageName} Link: ${img.imgUrl} data-skip=${img.dataSkip} ${img.postUrl}`)
+                    .map(img => 
+                        `${img.imageName}, Link: ${img.imgUrl}, data-skip=${img.dataSkip}, "${img.postTitle}", ${img.postUrl}`
+                    )
                     .join('\n');
+                    
                 fs.writeFileSync('./list_of_photos.txt', output);
                 return;
             }
 
             entries.forEach(entry => {
+                const postTitle = entry.title.$t.replace(/,/g, ''); // Remove commas to prevent CSV issues
                 const content = entry.content.$t;
                 const postUrl = entry.link.find(link => link.rel === 'alternate').href.replace(blogUrl, '');
                 const $ = cheerio.load(content);
@@ -47,7 +51,7 @@ function fetchData() {
                     imgSrc = imgSrc.replace(/\/s\d+\/|\/w\d+-h\d+\//, '/s400/');
                     const imageName = imgSrc.split('/').pop();
                     const dataSkip = $(img).attr('data-skip') || 'NA';
-                    imageDetailsBuffer.push({ imageName, imgUrl: imgSrc, dataSkip, postUrl });
+                    imageDetailsBuffer.push({ imageName, imgUrl: imgSrc, dataSkip, postTitle, postUrl });
                 });
             });
 
@@ -62,27 +66,30 @@ function processDuplicates(imageDetails) {
     const uniqueImages = [];
     const duplicates = [];
 
-    imageDetails.forEach(({ imageName, imgUrl, dataSkip, postUrl }) => {
+    imageDetails.forEach(({ imageName, imgUrl, dataSkip, postTitle, postUrl }) => {
         if (!filenameCounts[imageName]) {
-            filenameCounts[imageName] = { count: 0, urls: [], dataSkips: [], postUrls: [] };
+            filenameCounts[imageName] = { count: 0, urls: [], dataSkips: [], postTitles: [], postUrls: [] };
         }
         filenameCounts[imageName].count += 1;
         filenameCounts[imageName].urls.push(imgUrl);
         filenameCounts[imageName].dataSkips.push(dataSkip);
+        filenameCounts[imageName].postTitles.push(postTitle);
         filenameCounts[imageName].postUrls.push(postUrl);
 
         if (filenameCounts[imageName].count === 1) {
-            uniqueImages.push({ imageName, imgUrl, dataSkip, postUrl });
+            uniqueImages.push({ imageName, imgUrl, dataSkip, postTitle, postUrl });
         }
     });
 
     for (const [imageName, data] of Object.entries(filenameCounts)) {
         if (data.count > 1) {
             const combinedDataSkip = combineDataSkipValues(data.dataSkips);
+            const combinedPostTitle = [...new Set(data.postTitles)].join(' / '); // Merge unique titles
             const combinedPostUrl = [...new Set(data.postUrls)].join('; ');
             uniqueImages.forEach(img => {
                 if (img.imageName === imageName) {
                     img.dataSkip = combinedDataSkip;
+                    img.postTitle = combinedPostTitle;
                     img.postUrl = combinedPostUrl;
                 }
             });
